d1fb1d2acad8b0405c4b3ceb8a96a2aeeae7be52
grammlaman <grammlaman@gmail.com>
2019-12-18 18:22:48 +0600

    Главная готова

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,4 +1,18 @@
 $(function(){
+    let height = 0;
+    $(window).scroll(function(e) {
+        if (screen.availWidth >= 0){
+            height = 1;
+        } else if (screen.availWidth >= 1200) {
+            height = 112;
+        }
+        let scrolled = $(this).scrollTop();
+        if (scrolled >= height) {
+            $('#header').addClass('head-scroll');
+        } else {
+            $('#header').removeClass('head-scroll');
+        }
+    });
     $('.new-units').slick({
         mobileFirst: true,
         arrows: true,
@@ -19,7 +33,34 @@ $(function(){
             }
         ]
     });
-
+    $('.main-slider-cont').slick({
+        mobileFirst: true,
+        arrows: true,
+        dots: true,
+        dotsClass: 'main-slider-dots',
+        slidesToScroll: 1,
+        slidesToShow: 1,
+    });
+    $('.news-slider').slick({
+        mobileFirst: true,
+        arrows: true,
+        dots: false,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 760,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 300,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
 
 
 
